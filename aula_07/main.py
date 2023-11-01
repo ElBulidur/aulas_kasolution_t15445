072ca6aeca7e90d40bddceb0e32f1fca1ff3c228
Julio Pereira <juliopereira.dev@gmail.com>
2023-10-31 21:24:59 -0300

    aula 07 parte 01b

--- aula_07/main.py
+++ aula_07/main.py
@@ -1,28 +1,32 @@
 from manage import Manager
 
-trabalho = Manager()
+def start():
+    trabalho = Manager()
+    usuarios = [
+        ('Julio', 'biscoito'),
+        ('Andre', '234hnd'),
+        ('Marcello', 'jsaodsda'),
+        ('Fabio', 'jhsadnlsa')
+    ]
 
-# trabalho.pegar_conexao() # ativar conexao
+    # trabalho.pegar_conexao() # ativar conexao
 
-# trabalho.criar_usuario('Andre', 'Bolaquadrada') # criar usuario
+    # trabalho.criar_usuario('Andre', 'Bolaquadrada') # criar usuario
 
-# trabalho.atualizar_usuario('Pereira', 'bolinhadegude', 2) # atualizar usuario
+    # trabalho.atualizar_usuario('Pereira', 'bolinhadegude', 2) # atualizar usuario
 
-# trabalho.deletar_usuario(3) # deletar usuario
+    # trabalho.deletar_usuario(3) # deletar usuario
 
-usuarios = [
-    ('Julio', 'biscoito'),
-    ('Andre', '234hnd'),
-    ('Marcello', 'jsaodsda'),
-    ('Fabio', 'jhsadnlsa')
-]
 
-for usuario, senha in usuarios:
-    pass
-    # trabalho.pegar_conexao() 
-    # trabalho.criar_usuario(usuario, senha)
+    for usuario, senha in usuarios:
+        pass
+        # trabalho.pegar_conexao() 
+        # trabalho.criar_usuario(usuario, senha)
+        
+
+    trabalho.pegar_usuario_por_id(5)
     
-
-trabalho.pegar_usuario_por_id(5)
- 
-trabalho.pegar_todos_usuarios()
+    trabalho.pegar_todos_usuarios()
+    
+if __name__ == '__main__':
+    start()
